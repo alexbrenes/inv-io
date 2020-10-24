15578793a5b3b9d572964e65271d7fba74cd7220
Alex Brenes <brenes.brenes.alex@gmail.com>
2020-10-24 09:48:21 -0600

    TODO: fix queue

--- Scheme/sending_email.scm
+++ Scheme/sending_email.scm
@@ -8,7 +8,8 @@
 ;; (dijkstra '() 0 1 2)
 ;; Case #4
 ;; (dijkstra '((0 1 4) (1 0 4) (0 2 2) (2 0 2) (1 2 1) (2 1 1) (1 3 5) (3 1 5) (2 3 8) (3 2 8) (2 4 10) (4 2 10) (3 4 2) (4 3 2) (3 5 6) (5 3 6) (4 5 3) (5 4 3)) 5 2 6)
-
+;; Case #5
+;; (dijkstra '())
 
 (define r-edge
   (lambda (G)
@@ -56,14 +57,14 @@
 (define remplazar-dist
   (lambda (Dist V Q PA)
     (cond ((null? V) Dist)
-          (#t (remplazar-dist (replace-at Dist (+ (caar Q) (caddar V)) (caar V)) (cdr V) Q PA)))))
+          (#t (remplazar-dist (replace-at Dist (+ (element-at Dist (cadar Q)) (caddar V)) (caar V)) (cdr V) Q PA)))))
 
 (define dijkstra-aux
   (lambda (Q Dist G S D N)
     (cond ((null? Q) (element-at Dist D))
           ((equal? (cdar Q) D) (element-at Dist D))
-          (#t (dijkstra-aux (insertarV '() (filter (lambda (a) (< (+ (caddr a) (caar Q)) (element-at Dist (car a)))) (vecinos-de (cadar Q) G)))
-                            (remplazar-dist Dist (filter (lambda (a) (< (+ (caddr a) (caar Q)) (element-at Dist (car a)))) (vecinos-de (cadar Q) G)) Q (caar Q))
+          (#t (dijkstra-aux (insertarV (cdr Q) (filter (lambda (a) (< (+ (caddr a) (element-at Dist (cadar Q))) (element-at Dist (car a)))) (vecinos-de (cadar Q) G)))
+                            (remplazar-dist Dist (filter (lambda (a) (< (+ (caddr a) (element-at Dist (cadar Q))) (element-at Dist (car a)))) (vecinos-de (cadar Q) G)) Q (caar Q))
                             G S D N)))))
 
 (define dijkstra
@@ -74,23 +75,44 @@
 
 (define print
   (lambda (V Case)
-    (display "Case #") (display Case) (display ": ") (display (cond ((= V 1073741824) "unreachable")
-                                                                    (#t V)))))
+    (display "Case #")
+    (display Case)
+    (display ": ")
+    (display (cond ((= V 1073741824) "unreachable")
+                                                                    (#t V)))
+    (display "\n")))
 
 (define solve-aux
-  (lambda (input-file)
-    (tcase (string->number (read-line input-file)) file)))
+  (lambda (file)
+    (tcase (string->number (read-line file)) 1 file)))
+
+(define tcase-aux
+  (lambda (TC file)
+    (ttcase (read file) (read file) (read file) (read file) TC file)))
+
+(define edges
+  (lambda (A file)
+    (cond ((= A 0) '())
+          (#t (append (list (append (list (read file)) (list (read file)) (list (read file)))) (edges (- A 1) file))))))
+
+(define ttcase
+  (lambda (N A S D TC file)
+    (print (dijkstra (r-edge (edges A file)) S D N) TC)))
 
 (define tcase
-  (lambda (T file)
-    (cond ((= T 1) (tcase-aux file))
-          (#t (tcase (- T 1) )))))
+  (lambda (T TC file)
+    (cond ((= T 1) (tcase-aux TC file))
+          (#t (tcase-aux TC file)
+              (tcase (- T 1) (+ TC 1) file)))))
 
 (define solve
   (lambda (file)
     (solve-aux (open-input-file file))))
-(print (dijkstra '((0 1 4) (1 0 4) (0 2 2) (2 0 2) (1 2 1) (2 1 1) (1 3 5) (3 1 5) (2 3 8) (3 2 8) (2 4 10) (4 2 10) (3 4 2) (4 3 2) (3 5 6) (5 3 6) (4 5 3) (5 4 3)) 5 2 6) 1)
-
+(solve "se.txt")
+;; (print (dijkstra '((0 1 4) (1 0 4) (0 2 2) (2 0 2) (1 2 1) (2 1 1) (1 3 5) (3 1 5) (2 3 8) (3 2 8) (2 4 10) (4 2 10) (3 4 2) (4 3 2) (3 5 6) (5 3 6) (4 5 3) (5 4 3)) 5 2 6) 1)
+;; (print (dijkstra (r-edge '((0 1 50) (2 1 70) (0 2 150))) 0 2 6) 1)
+;; (print (dijkstra (r-edge '((1 0 10000))) 0 1 2) 1)
 ;; (print (dijkstra '((0 1 100) (1 0 100)) 0 2 3) 1)
 ;; (print (dijkstra '((0 1 100) (0 2 200) (1 0 100) (1 2 50) (2 0 200) (2 1 50)) 2 0 3) 1)
 ;; (print (dijkstra '() 0 1 2) 1)
+;; (print (dijkstra (r-edge '((0 1 123) (1 2 467) (2 3 478) (4 5 578) (5 3 47) (2 6 256) (7 2 246) (5 9 245) (5 3 1245) (8 2 575) (9 4 235) (2 8 437) (9 12 367) (13 7 587) (11 9 476) (9 5 34) (11 13 35) (13 10 2) (12 11 7) (12 6 235))) 13 2 14) 1)
